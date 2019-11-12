40728eb452743fbccad631557632d5dd4f2bfb66
João Henrique <joaohenrique.p.almeida@gmail.com>
2019-11-12 06:11:25 -0300

    bugfix -  Change User to Account

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-
-  resources :users, param: :_cpf
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :accounts, param: :_cpf
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_011207) do
+ActiveRecord::Schema.define(version: 2019_11_12_083951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.string "cpf"
     t.string "email"
